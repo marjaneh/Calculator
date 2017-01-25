b3803a6b4b9d34c36d50eea8277560e5cbd61a0c
Marjaneh jamali <arm_jamali2000@yahoo.com>
2017-01-24 20:07:27 -0500

    Calculator Project

---
+++ Calculator.js
@@ -0,0 +1,27 @@
+﻿/*
+ * created by Marjaneh
+ * options: 
+ *      
+ */
+"use strict"
+
+var Calculator = function (options) {
+    var Self = this;
+    var screen,
+        digits,
+        operators,
+        controllers;
+
+    Self.Init = function () {
+        Self.screen         = options.Screen;
+        Self.digits         = options.Digits;
+        Self.operators      = options.Operations;
+        Self.controllers    = operators.Controllers;
+    };
+
+    Self.ExtendToAdvance = function () {
+        
+    };
+}
+
+
